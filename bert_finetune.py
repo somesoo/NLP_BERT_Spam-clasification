280e71100802da7edb554e737ce4563aa3ff5273
somesoo <jakasperczak@gmail.com>
2025-01-06 18:27:19 +0100

    cleaning whitespaces

--- bert_finetune.py
+++ bert_finetune.py
@@ -22,8 +22,6 @@ import ssl
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler,random_split
 from transformers import BertForSequenceClassification, AdamW, BertConfig,BertTokenizer,get_linear_schedule_with_warmup
 
-
-
 # this part is downloading stopwoards to clean dataset before learning
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -35,21 +33,15 @@ else:
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
-
 if (config.PARAM1 == "true"):
     device = torch.device("cpu")
 else:
     device = "cuda:0" 
-#if torch.cuda.is_available() else torch.device("cpu")
-
 print(device)
-
 
 # This part is responsible for importing and preparing data
 df_training = pd.read_csv(config.PARAM2)
 df_test = pd.read_csv(config.PARAM3)
-
-
 
 sw = stopwords.words('english')
 
@@ -91,14 +83,9 @@ tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 seq_len = [len(i.split()) for i in train_text]
 hist, bin_edges = np.histogram(seq_len, bins=len(seq_len))
 hist_table = pd.DataFrame({'Message lenght': [f"{int(bin_edges[i])} - {int(bin_edges[i+1])}" for i in range(len(bin_edges)-1)],'Frequency': hist})
-#print(hist_table)
-
-# Calculate cumulative frequency
 cumulative_frequency = np.cumsum(hist)
-
 # Total frequency
 total_frequency = cumulative_frequency[-1]
-
 # Find the bin edge that covers the desired percentage
 cutoff_frequency = total_frequency * 0.9
 bin_index = np.searchsorted(cumulative_frequency, cutoff_frequency)
@@ -108,7 +95,6 @@ if int(bin_index)+1 > 512:
     max_seq_len = 512
 else:
    max_seq_len = int(bin_edges[bin_index + 1])
-
 print(max_seq_len)
 
 # tokenize and encode sequences in the training set
@@ -152,9 +138,6 @@ val_y = torch.tensor(val_labels.tolist())
 test_seq = torch.tensor(tokens_test['input_ids'])
 test_mask = torch.tensor(tokens_test['attention_mask'])
 test_y = torch.tensor(test_labels.tolist())
-
-
-
 
 #define a batch size
 batch_size = config.PARAM7
@@ -207,119 +190,80 @@ def evaluate():
 
   # deactivate dropout layers
   model.eval()
-
   total_loss, total_accuracy = 0, 0
-
   # empty list to save the model predictions
   total_preds = []
-
   t0 = time.time()
-
   # iterate over batches
   for step,batch in enumerate(val_dataloader):
-
     # Progress update every 50 batches.
     if step % 50 == 0 and not step == 0:
-
       # Calculate elapsed time in minutes.
       elapsed = format_time(time.time() - t0)
       t0 = time.time()
       # Report progress.
       print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(val_dataloader)))
-
-    # push the batch to gpu
+      print(elapsed)
+    # push the batch to device
     batch = [t.to(device) for t in batch]
-
     sent_id, mask, labels = batch
-
     # deactivate autograd
     with torch.no_grad():
-
       # model predictions
       preds = model(sent_id, mask)
-
       # compute the validation loss between actual and predicted values
       loss = cross_entropy(preds,labels)
-
       total_loss = total_loss + loss.item()
-
       preds = preds.detach().cpu().numpy()
-
       total_preds.append(preds)
-
   # compute the validation loss of the epoch
   avg_loss = total_loss / len(val_dataloader)
-
   # reshape the predictions in form of (number of samples, no. of classes)
   total_preds  = np.concatenate(total_preds, axis=0)
-
   return avg_loss, total_preds
 
 # function to train the model
 def train():
-
   model.train()
-
   total_loss, total_accuracy = 0, 0
-
   # empty list to save model predictions
   total_preds=[]
-
   # iterate over batches
   for step,batch in enumerate(train_dataloader):
-
     # progress update after every 50 batches.
     if step % 50 == 0 and not step == 0:
       print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(train_dataloader)))
-
-    # push the batch to gpu
+    # push the batch to device
     batch = [r.to(device) for r in batch]
-
     sent_id, mask, labels = batch
-
     # clear previously calculated gradients
     model.zero_grad()
-
     # get model predictions for the current batch
     preds = model(sent_id, mask)
-
     # compute the loss between actual and predicted values
     loss = cross_entropy(preds, labels)
-
     # add on to the total loss
     total_loss = total_loss + loss.item()
-
     # backward pass to calculate the gradients
     loss.backward()
-
     # clip the the gradients to 1.0. It helps in preventing the exploding gradient problem
     torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-
     # update parameters
     optimizer.step()
-
     # model predictions are stored on GPU. So, push it to CPU
     preds=preds.detach().cpu().numpy()
-
     # append the model predictions
     total_preds.append(preds)
-
   # compute the training loss of the epoch
   avg_loss = total_loss / len(train_dataloader)
-
   # predictions are in the form of (no. of batches, size of batch, no. of classes).
   # reshape the predictions in form of (number of samples, no. of classes)
   total_preds  = np.concatenate(total_preds, axis=0)
-
   #returns the loss and predictions
   return avg_loss, total_preds
 
-
-
-
 # set initial loss to infinite
 best_valid_loss = float('inf')
-
 # empty lists to store training and validation loss of each epoch
 train_losses=[]
 valid_losses=[]
@@ -329,22 +273,17 @@ if config.PARAM10:
     for epoch in range(epochs):
 
         print('\n Epoch {:} / {:}'.format(epoch + 1, epochs))
-
         #train model
         train_loss, _ = train()
-
         #evaluate model
         valid_loss, _ = evaluate()
-
         #save the best model
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             torch.save(model.state_dict(), 'saved_weights.pt')
-
         # append training and validation loss
         train_losses.append(train_loss)
         valid_losses.append(valid_loss)
-
         print(f'\nTraining Loss: {train_loss:.3f}')
         print(f'Validation Loss: {valid_loss:.3f}')
 
